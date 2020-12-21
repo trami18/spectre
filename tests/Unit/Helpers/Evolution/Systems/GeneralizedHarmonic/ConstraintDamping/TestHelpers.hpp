a9910129e9c38671dd075c0573b664ad133315bc
Geoffrey Lovelace <geoffrey4444@gmail.com>
2020-12-20 18:44:10 -0800

    Support time dependence in DampingFunction TestHelpers

--- tests/Unit/Helpers/Evolution/Systems/GeneralizedHarmonic/ConstraintDamping/TestHelpers.hpp
+++ tests/Unit/Helpers/Evolution/Systems/GeneralizedHarmonic/ConstraintDamping/TestHelpers.hpp
@@ -5,6 +5,7 @@
 
 #include "Framework/TestingFramework.hpp"
 
+#include <algorithm>
 #include <array>
 #include <limits>
 #include <memory>
@@ -37,6 +38,7 @@ void check_impl(
             VolumeDim, Fr>>& in_gh_damping_function,
     const std::string& python_function_prefix, const T& used_for_size,
     const std::array<std::pair<double, double>, 1> random_value_bounds,
+    const std::string& function_of_time_name,
     const MemberArgs&... member_args) noexcept {
   using GhDampingFunc =
       ::GeneralizedHarmonic::ConstraintDamping::DampingFunction<VolumeDim, Fr>;
@@ -44,30 +46,44 @@ void check_impl(
   const auto member_args_tuple = std::make_tuple(member_args...);
   const auto helper =
       [&python_function_prefix, &random_value_bounds, &member_args_tuple,
-       &used_for_size](
+       &function_of_time_name, &used_for_size](
           const std::unique_ptr<GhDampingFunc>& gh_damping_function) noexcept {
         INFO("Testing call operator...")
-
         // Make a lambda that calls the damping function's call operator
         // with a hard-coded FunctionsOfTime, since check_with_random_values
         // cannot convert a FunctionsOfTime into a python type.
         // The FunctionsOfTime contains a single FunctionOfTime
-        // \f$f(t) = a_0 + a_1 t + a_2 t^2 + a_3 t^3\f$, where
-        // \f$a_0 = 1.0\f$, \f$a_1 = 0.2\f$, \f$a_3 = 0.03,\f$ and
-        // \f$a_4 = 0.004\f$. The corresponding python function should use
-        // the same hard-coded coefficients to evaluate \f$f(t)\f$.
-        // Note that the FunctionOfTime never expires.
+        // \f$f(t) = a_0 + a_1 (t-t_0) + a_2 (t-t_0)^2 + a_3 (t-t_0)^3\f$, where
+        // \f$a_0 = 1.0\f$, \f$a_1 = 0.2\f$, \f$a_2 = 0.03,\f$,
+        // \f$a_3 = 0.004\f$, and \f$t_0\f$ is the smallest possible value
+        // of the randomly selected time.
+        //
+        // The corresponding python function should use
+        // the same hard-coded coefficients to evaluate \f$f(t)\f$ as well
+        // as the same value of \f$t_0\f$.
+        // However, here the PiecewisePolynomial must be initialized not
+        // with the polynomial coefficients but with the values of \f$f(t)\f$
+        // and its derivatives evaluated at \f$t=t_0\f$: these are,
+        // respectively, \f$a_0,a_1,2 a_2, 6_a3\f$.
+        //
+        // Finally, note that the FunctionOfTime never expires.
         const auto damping_function_call_operator_helper =
-            [&gh_damping_function](const tnsr::I<T, VolumeDim, Fr>& coordinates,
-                                   const double time) {
+            [&gh_damping_function, &random_value_bounds,
+             &function_of_time_name](
+                const tnsr::I<T, VolumeDim, Fr>& coordinates,
+                const double time) {
               std::unordered_map<
                   std::string,
                   std::unique_ptr<::domain::FunctionsOfTime::FunctionOfTime>>
                   functions_of_time{};
-              functions_of_time["ExpansionFactor"] = std::make_unique<
+              // The randomly selected time will be between the
+              // random_value_bounds, so set the earliest time of the
+              // function_of_time to the lower bound in random_value_bounds.
+              functions_of_time[function_of_time_name] = std::make_unique<
                   ::domain::FunctionsOfTime::PiecewisePolynomial<3>>(
-                  0.0,
-                  std::array<DataVector, 4>{{{1.0}, {0.2}, {0.03}, {0.004}}},
+                  std::min(gsl::at(random_value_bounds, 0).first,
+                           gsl::at(random_value_bounds, 0).second),
+                  std::array<DataVector, 4>{{{1.0}, {0.2}, {0.06}, {0.024}}},
                   std::numeric_limits<double>::max());
               // Default-construct the scalar, to test that the damping
               // function's call operator correctly resizes it
@@ -105,6 +121,13 @@ void check_impl(
  * DampingFunction is arbitrary, but should generally be descriptive (e.g.
  * 'gaussian_plus_constant') of the DampingFunction.
  *
+ * The input parameter `function_of_time_name` is the name of the FunctionOfTime
+ * that will be included in the FunctionsOfTime passed to the DampingFunction's
+ * call operator. For time-dependent FunctionsOfTime, this parameter must be
+ * consistent with the FunctionOfTime name that in_gh_damping_function's call
+ * operator expects. For time-independent FunctionsOfTime,
+ * `function_of_time_name` will be ignored.
+ *
  * If a DampingFunction class has member variables set by its constructor, then
  * these member variables must be passed in as the last arguments to the `check`
  * function`. Each python function must take these same arguments as the
@@ -114,31 +137,31 @@ template <class DampingFunctionType, class T, class... MemberArgs>
 void check(std::unique_ptr<DampingFunctionType> in_gh_damping_function,
            const std::string& python_function_prefix, const T& used_for_size,
            const std::array<std::pair<double, double>, 1>& random_value_bounds,
+           const std::string& function_of_time_name,
            const MemberArgs&... member_args) noexcept {
   detail::check_impl(
-      std::unique_ptr<
-          ::GeneralizedHarmonic::ConstraintDamping::DampingFunction<
-              DampingFunctionType::volume_dim,
-              typename DampingFunctionType::frame>>(
+      std::unique_ptr<::GeneralizedHarmonic::ConstraintDamping::DampingFunction<
+          DampingFunctionType::volume_dim,
+          typename DampingFunctionType::frame>>(
           std::move(in_gh_damping_function)),
       python_function_prefix, used_for_size, random_value_bounds,
-      member_args...);
+      function_of_time_name, member_args...);
 }
 
 template <class DampingFunctionType, class T, class... MemberArgs>
 void check(DampingFunctionType in_gh_damping_function,
            const std::string& python_function_prefix, const T& used_for_size,
            const std::array<std::pair<double, double>, 1>& random_value_bounds,
+           const std::string& function_of_time_name,
            const MemberArgs&... member_args) noexcept {
   detail::check_impl(
-      std::unique_ptr<
-          ::GeneralizedHarmonic::ConstraintDamping::DampingFunction<
-              DampingFunctionType::volume_dim,
-              typename DampingFunctionType::frame>>(
+      std::unique_ptr<::GeneralizedHarmonic::ConstraintDamping::DampingFunction<
+          DampingFunctionType::volume_dim,
+          typename DampingFunctionType::frame>>(
           std::make_unique<DampingFunctionType>(
               std::move(in_gh_damping_function))),
       python_function_prefix, used_for_size, random_value_bounds,
-      member_args...);
+      function_of_time_name, member_args...);
 }
 // @}
 }  // namespace TestHelpers::GeneralizedHarmonic::ConstraintDamping
